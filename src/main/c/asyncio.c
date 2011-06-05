0cfbcf396b3ca7f832800fee436df45a9a573da1
Joe Walnes <jwalnes@drwholdings.com>
2011-06-05 10:20:40 -0500

    Ability to access request from callback;

--- src/main/c/asyncio.c
+++ src/main/c/asyncio.c
@@ -5,9 +5,17 @@
 #include <jni.h>
 #include <eio.h>
 
-#define JNI_METHOD(returnType, name) JNIEXPORT returnType JNICALL Java_org_webbitserver_asyncio_AsyncIO_##name
+#define JNI_API(returnType, name) \
+  JNIEXPORT returnType JNICALL \
+  Java_org_webbitserver_asyncio_AsyncIO_##name
+
+#define JNI_REQ(returnType, name) \
+  JNIEXPORT returnType JNICALL \
+  Java_org_webbitserver_asyncio_RequestWrapper_##name
 
 jmethodID callback_jmethod;
+jclass req_class;
+jmethodID req_constructor;
 struct pollfd pfd;
 int pipe_out, pipe_in;
 
@@ -21,12 +29,19 @@ void done_poll(void) {
 	read(pipe_out, &dummy, 1);
 }
 
-JNI_METHOD(void, init)(JNIEnv *env, jobject self) {
-	jclass cls = (*env)->FindClass(env, "org/webbitserver/asyncio/AioCallback");
-	assert(cls != NULL);
-	callback_jmethod = (*env)->GetMethodID(env, cls,
+JNI_API(void, init)(JNIEnv *env, jobject self) {
+	jclass callback_cls = (*env)->FindClass(env, "org/webbitserver/asyncio/AioCallback");
+	assert(callback_cls != NULL);
+	callback_jmethod = (*env)->GetMethodID(env, callback_cls,
 			"complete", "(Lorg/webbitserver/asyncio/AioRequest;)V");
 	assert(callback_jmethod != NULL);
+
+  jclass req_class_local = (*env)->FindClass(env, "org/webbitserver/asyncio/RequestWrapper");
+	req_class = (*env)->NewGlobalRef(env, req_class_local);
+	assert(req_class != NULL);
+	req_constructor = (*env)->GetMethodID(env, req_class,
+			"<init>", "(J)V");
+	assert(req_constructor != NULL);
 
   int pipe_args[2];
 	assert(pipe(pipe_args) == 0);
@@ -38,14 +53,14 @@ JNI_METHOD(void, init)(JNIEnv *env, jobject self) {
 	assert(eio_init(want_poll, done_poll) == 0);
 }
 
-JNI_METHOD(jint, poll)(JNIEnv *env, jobject self) { return eio_poll(); } 
-JNI_METHOD(void, block)(JNIEnv *env, jobject self) { poll(&pfd, 1, -1); }
-JNI_METHOD(jint, wakeUpFileDescriptor)(JNIEnv *env, jobject self) { return pipe_out; }
+JNI_API(jint, poll)(JNIEnv *env, jobject self) { return eio_poll(); } 
+JNI_API(void, block)(JNIEnv *env, jobject self) { poll(&pfd, 1, -1); }
+JNI_API(jint, wakeUpFileDescriptor)(JNIEnv *env, jobject self) { return pipe_out; }
 
-JNI_METHOD(jint, numRequests)(JNIEnv *env, jobject self) { return eio_nreqs(); }
-JNI_METHOD(jint, numReady   )(JNIEnv *env, jobject self) { return eio_nready(); }
-JNI_METHOD(jint, numPending )(JNIEnv *env, jobject self) { return eio_npending(); }
-JNI_METHOD(jint, numThreads )(JNIEnv *env, jobject self) { return eio_nthreads(); }
+JNI_API(jint, numRequests)(JNIEnv *env, jobject self) { return eio_nreqs(); }
+JNI_API(jint, numReady   )(JNIEnv *env, jobject self) { return eio_nready(); }
+JNI_API(jint, numPending )(JNIEnv *env, jobject self) { return eio_npending(); }
+JNI_API(jint, numThreads )(JNIEnv *env, jobject self) { return eio_nthreads(); }
 
 struct java_callback {
 	JNIEnv* env;
@@ -77,36 +92,98 @@ int completion_callback(eio_req *req) {
 
 	JNIEnv* env = cb->env;
 
-	(*env)->CallObjectMethod(env, cb->global, callback_jmethod, NULL);
+  jobject java_request = (*env)->NewObject(
+      env, req_class, req_constructor, (jlong)req);
+	(*env)->CallObjectMethod(
+      env, cb->global, callback_jmethod, java_request);
 
 	free_java_callback(cb);
 }
 
-JNI_METHOD(void, open)(JNIEnv *env, jobject self, jstring path, jint flag, jint mode, jint priority, jobject callback) {
+JNI_API(void, open)(JNIEnv *env, jobject self, jstring path, jint flag, jint mode, jint priority, jobject callback) {
 	const char* pathChars = (*env)->GetStringUTFChars(env, path, 0);
 	eio_open(pathChars, flag, mode, priority, completion_callback, alloc_java_callback(env, callback));
 	(*env)->ReleaseStringUTFChars(env, path, pathChars);
 }
 
-JNI_METHOD(void, close)(JNIEnv *env, jobject self, jint fd, jint priority, jobject callback) {
+JNI_API(void, close)(JNIEnv *env, jobject self, jint fd, jint priority, jobject callback) {
 	eio_close(fd, priority, completion_callback, alloc_java_callback(env, callback));
 }
 
-JNI_METHOD(void, unlink)(JNIEnv *env, jobject self, jstring path, jint priority, jobject callback) {
+JNI_API(void, unlink)(JNIEnv *env, jobject self, jstring path, jint priority, jobject callback) {
 	const char* pathChars = (*env)->GetStringUTFChars(env, path, 0);
 	eio_unlink(pathChars, priority, completion_callback, alloc_java_callback(env, callback));
 	(*env)->ReleaseStringUTFChars(env, path, pathChars);
 }
 
-JNI_METHOD(void, mkdir)(JNIEnv *env, jobject self, jstring path, jint mode, jint priority, jobject callback) {
+JNI_API(void, mkdir)(JNIEnv *env, jobject self, jstring path, jint mode, jint priority, jobject callback) {
 	const char* pathChars = (*env)->GetStringUTFChars(env, path, 0);
 	eio_mkdir(pathChars, mode, priority, completion_callback, alloc_java_callback(env, callback));
 	(*env)->ReleaseStringUTFChars(env, path, pathChars);
 }
 
-JNI_METHOD(void, rmdir)(JNIEnv *env, jobject self, jstring path, jint priority, jobject callback) {
+JNI_API(void, rmdir)(JNIEnv *env, jobject self, jstring path, jint priority, jobject callback) {
 	const char* pathChars = (*env)->GetStringUTFChars(env, path, 0);
 	eio_rmdir(pathChars, priority, completion_callback, alloc_java_callback(env, callback));
 	(*env)->ReleaseStringUTFChars(env, path, pathChars);
 }
 
+JNI_REQ(jint, result)(JNIEnv *env, jobject self, jlong ptr) {
+  eio_req* req = (eio_req*)ptr;
+  return req->result;
+}
+
+JNI_REQ(jint, offs)(JNIEnv *env, jobject self, jlong ptr) {
+  eio_req* req = (eio_req*)ptr;
+  return req->offs;
+}
+
+JNI_REQ(jint, size)(JNIEnv *env, jobject self, jlong ptr) {
+  eio_req* req = (eio_req*)ptr;
+  return req->size;
+}
+
+JNI_REQ(jlong, ptr1)(JNIEnv *env, jobject self, jlong ptr) {
+  eio_req* req = (eio_req*)ptr;
+  return (jlong)req->ptr1;
+}
+
+JNI_REQ(jlong, ptr2)(JNIEnv *env, jobject self, jlong ptr) {
+  eio_req* req = (eio_req*)ptr;
+  return (jlong)req->ptr2;
+}
+
+JNI_REQ(jlong, nv1)(JNIEnv *env, jobject self, jlong ptr) {
+  eio_req* req = (eio_req*)ptr;
+  return req->nv1;
+}
+
+JNI_REQ(jlong, nv2)(JNIEnv *env, jobject self, jlong ptr) {
+  eio_req* req = (eio_req*)ptr;
+  return req->nv2;
+}
+
+JNI_REQ(jint, type)(JNIEnv *env, jobject self, jlong ptr) {
+  eio_req* req = (eio_req*)ptr;
+  return req->type;
+}
+
+JNI_REQ(jint, int1)(JNIEnv *env, jobject self, jlong ptr) {
+  eio_req* req = (eio_req*)ptr;
+  return req->int1;
+}
+
+JNI_REQ(jlong, int2)(JNIEnv *env, jobject self, jlong ptr) {
+  eio_req* req = (eio_req*)ptr;
+  return req->int2;
+}
+
+JNI_REQ(jlong, int3)(JNIEnv *env, jobject self, jlong ptr) {
+  eio_req* req = (eio_req*)ptr;
+  return req->int3;
+}
+
+JNI_REQ(jint, errorno)(JNIEnv *env, jobject self, jlong ptr) {
+  eio_req* req = (eio_req*)ptr;
+  return req->errorno;
+}
---
+++ src/main/java/org/webbitserver/asyncio/RequestWrapper.java
@@ -0,0 +1,63 @@
+package org.webbitserver.asyncio;
+
+public class RequestWrapper implements AioRequest {
+
+  private final long ptr;
+
+  public RequestWrapper(long ptr) {
+    this.ptr = ptr;
+  }
+
+  public long ptr() {
+    return ptr;
+  }
+
+  @Override
+  public boolean success() {
+    return result(ptr) > -1;
+  }
+
+  /** result of syscall, e.g. result = read (... */
+  public static native int result(long ptr);
+
+  /** read, write, truncate, readahead, 
+   *  sync_file_range: file offset, mknod: dev_t */
+  public static native int offs(long ptr);
+
+  /** read, write, readahead, sendfile, msync, 
+   *  mlock, sync_file_range: length */
+  public static native int size(long ptr);
+
+  /** all applicable requests: pathname, 
+   *  old name; readdir: optional eio_dirents */
+  public static native long ptr1(long ptr);
+
+  /** all applicable requests: new name or memory buffer;
+   *  readdir: name strings */
+  public static native long ptr2(long ptr);
+
+  /** utime, futime: atime; busy: sleep time */
+  public static native long nv1(long ptr);
+
+  /** utime, futime: mtime */
+  public static native long nv2(long ptr);
+
+  /** EIO_xxx constant ETP */
+  public static native int type(long ptr);
+
+  /** all applicable requests: file descriptor;
+   *  sendfile: output fd; open, msync, mlockall, readdir: flags */
+  public static native int int1(long ptr);
+
+  /** chown, fchown: uid; sendfile: input fd; 
+   *  open, chmod, mkdir, mknod: file mode, sync_file_range: flags */
+  public static native long int2(long ptr);
+
+  /* chown, fchown: gid */
+  public static native long int3(long ptr);
+
+  /* errno value on syscall return */
+  public static native int errorno(long ptr);
+
+}
+
